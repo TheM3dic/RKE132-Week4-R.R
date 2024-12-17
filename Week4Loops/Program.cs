361a2c00e3fd173ac3e4d72872633301957c2938
The Medic <rein.reinstein@tktk.ee>
2024-12-17 22:10:36 +0200

    add Week4Loops code

---
+++ Week4Loops/Program.cs
@@ -0,0 +1,11 @@
+﻿
+//FOR-LOOP
+//DRY-principle - Do not repeat yourself
+
+int i = 0;
+for( i = 1; i < 5; i++) //i++ = i + 1
+{
+    Console.WriteLine("we Don't need education");
+}
+
+Console.WriteLine(i);
